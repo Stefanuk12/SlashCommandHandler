e3b8f0c7329718c4208af757e26010a0eaa4c6a5
Stefanuk12 <42220813+Stefanuk12@users.noreply.github.com>
2022-08-21 14:32:26 +0100

    Tidying up and fixing bugs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // Dependencies
-import { ChatInputCommandInteraction, ContextMenuCommandInteraction } from "discord.js";
+import { ChatInputCommandInteraction, CommandInteraction, ContextMenuCommandInteraction } from "discord.js";
 import { Command, ICommand } from "./modules/Command.js";
 import { AddCommand, Commands, GetCommand, GetContextCommand, GetContextCommands, GetSlashCommands, IImportFormat, InitialiseCommands } from "./modules/Commands.js";
 import { IPermissionHandler, PermissionHandler } from "./modules/PermissionHandler.js";
@@ -8,16 +8,7 @@ import { IPermissions, Permissions } from "./modules/Permissions.js";
 import { IRolePermissionHandler, RolePermissionHandler } from "./modules/RolePermissionHandler.js";
 
 // Listener that handles all the important stuff
-async function _CommandInteractionListener(interaction: ChatInputCommandInteraction){
-    // Attempt to get the command
-    let SubcommandGroup = interaction.options.getSubcommandGroup()
-    let SubcommandCommand = interaction.options.getSubcommand()
-    const Command = GetCommand([interaction.commandName, SubcommandGroup, SubcommandCommand])
-
-    if (!Command){
-        throw(new Error("Invalid command"))
-    }
-
+async function HandleInteraction(interaction: CommandInteraction, Command: Command) {
     // Permission Check
     if (Command.Permissions){
         const HasPermission = await Command.Permissions.check(interaction.user)
@@ -32,16 +23,28 @@ async function _CommandInteractionListener(interaction: ChatInputCommandInteract
 }
 
 // Listener that has failsafes
-async function CommandInteractionListener(interaction: ChatInputCommandInteraction){
-    // Defer the reply so we have time
-    if (!interaction.deferred)
+async function CommandInteractionListener(interaction: ChatInputCommandInteraction | ContextMenuCommandInteraction){
+    try {
+        // Make sure the command exists first
+        let SubcommandGroup = null
+        let SubcommandCommand = null
+        if (interaction instanceof ChatInputCommandInteraction) {
+            SubcommandGroup = interaction.options.getSubcommandGroup()
+            SubcommandCommand = interaction.options.getSubcommand()
+        }
+        const Command = GetCommand([interaction.commandName, SubcommandGroup, SubcommandCommand])
+
+        if (!Command){
+            throw(new Error("Invalid command"))
+        }
+
+        // Defer the reply so we have time
+        if (!interaction.deferred)
         await interaction.deferReply({
             ephemeral: true
         })
 
-    // Attempt to handle
-    try {
-        await _CommandInteractionListener(interaction)
+        await HandleInteraction(interaction, Command)
         return true
     } catch (error: any) {
         // Create Embed
@@ -57,52 +60,8 @@ async function CommandInteractionListener(interaction: ChatInputCommandInteracti
 }
 export default CommandInteractionListener
 
-//
-async function _ContextInteractionListener(interaction: ContextMenuCommandInteraction) {
-    // Attempt to get the command
-    const Command = GetContextCommand([interaction.commandName])
-
-    if (!Command){
-        throw(new Error("Invalid command"))
-    }
-
-    // Permission Check
-    if (Command.Permissions){
-        const HasPermission = await Command.Permissions.check(interaction.user)
-        if (!HasPermission){
-            throw(new Error("Missing permissions"))
-        }
-    }
-
-    //
-    if (Command.Callback)
-        await Command.Callback(interaction)
-}
-
-// Listener for context commands
-export async function ContextInteractionListener(interaction: ContextMenuCommandInteraction) {
-    // Defer the reply so we have time
-    if (!interaction.deferred)
-        await interaction.deferReply({
-            ephemeral: true
-        })
-
-    // Attempt to handle
-    try {
-        await _ContextInteractionListener(interaction)
-        return true
-    } catch (error: any) {
-        // Create Embed
-        const Embed = CreateBaseEmbed("Error", interaction.user)
-            .setDescription(error.message);
-
-        // Send
-        await interaction.editReply({ embeds: [Embed] })
-
-        // Return
-        return false
-    }
-}
+// Listener for context commands (kept so stuff doesn't break)
+export const ContextInteractionListener = CommandInteractionListener 
 
 // Exports
 export { Command, ICommand }
